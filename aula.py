90cf9c7fa986e8af2f1dc9fb05e829367ea15cde
Diogo de Souza <diogo@sozua.dev>
2021-01-25 15:41:34 -0300

    04 - Valores e tipos em Python

--- aula.py
+++ aula.py
@@ -1,5 +1,21 @@
-a = 10
-b = 20
+print(type(10)) # <class 'int'>
+print(type(10)) # <class 'int'>
+print(type(5 / 2)) # <class 'float'>
 
-print("A soma dos números é:", a + b)
+peso = 78
+altura = 1.78
 
+imc = peso / (altura ** 2)
+
+print(imc)
+print(type(imc)) # <class 'float'>
+
+imcInteiro = int(imc)
+
+print(imcInteiro)
+print(type(imcInteiro))
+
+temp = str(imc)
+print(temp)
+print(type(temp))
+print(len(temp)) # Comprimento do float
