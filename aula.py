25c58fa7d912c6988b10a399a8a5fce1d31cb95d
Diogo de Souza <diogo@sozua.dev>
2021-01-25 16:10:15 -0300

    05 - Entrada de dados

--- aula.py
+++ aula.py
@@ -1,21 +1,5 @@
-print(type(10)) # <class 'int'>
-print(type(10)) # <class 'int'>
-print(type(5 / 2)) # <class 'float'>
+temperaturaFahrenheit = input('Digite uma temperatura em Fahrenheit: ')
 
-peso = 78
-altura = 1.78
+temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5 / 9
 
-imc = peso / (altura ** 2)
-
-print(imc)
-print(type(imc)) # <class 'float'>
-
-imcInteiro = int(imc)
-
-print(imcInteiro)
-print(type(imcInteiro))
-
-temp = str(imc)
-print(temp)
-print(type(temp))
-print(len(temp)) # Comprimento do float
+print("A temperatura em Celsius é: ", temperaturaCelsius)
