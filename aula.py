2fdf9a096d4a28f492906bbebe71602d653e9a66
Diogo de Souza <diogo@sozua.dev>
2021-01-28 12:21:51 -0300

    25 - Matrizes

--- aula.py
+++ aula.py
@@ -1,55 +1,25 @@
-def cloneArray(array):
-    clone = []
-    for item in array:
-        clone.append(item)
-    return clone
+def criarMatriz(linhas, colunas):
+    matriz = []
+
+    for _ in range(linhas):
+        linha = []
+        for i in range(colunas):
+            linha.append(i)
+        matriz.append(linha)
+
+    return matriz
 
 
-def cloneArrayAlt(array):
-    return array[:]
+def criarMatrizComTeclado():
+    linhas = int(input("Digite o número de linhas da matriz: "))
+    colunas = int(input("Digite o número de colunas da matriz: "))
 
-
-def clonandoArrays():
-    firstOriginalArray = ['Vermelho', "Azul", "Rosa"]
-    secondOriginalArray = ['Vermelho', "Azul", "Rosa"]
-    firstCloneArray = cloneArray(firstOriginalArray)
-    secondCloneArray = cloneArrayAlt(firstOriginalArray)
-
-    firstCloneArray[0] = "Verde"
-    secondCloneArray[0] = "Verde"
-
-    print("Originais: \n")
-    print(firstOriginalArray)
-    print(secondOriginalArray)
-    print("\n Clones: \n")
-    print(firstCloneArray)
-    print(secondCloneArray)
-    print()
-
-
-def pertinenciaArray():
-    if "Vermelho" in ["Vermelho", "Azul"]:
-        print("Vermelho está na lista")
-    else:
-        print("Vermelho não está na lista")
-
-
-def concatenacaoArray():
-    firstArray = [1, 2]
-    secondArray = [3, 4]
-
-    finalArray = firstArray + secondArray
-    print(finalArray)
-
-
-def repeticaoArray():
-    a = [1, 2, 3]
-    aTriplicado = a * 3
-    print(aTriplicado)
-
-
-def removendoItensArray():
-    lista = [1, 2, 3, 4, 5]
-    print("Antes: ", lista)
-    del lista[0:3]
-    print("Depois: ", lista)
+    matriz = []
+    for linha in range(linhas):
+        linhaArray = []
+        for coluna in range(colunas):
+            inputValue = input('Digite o valor da coluna ' +
+                               str((coluna + 1)) + ' na linha ' + str((linha + 1)) + ': ')
+            linhaArray.append(inputValue)
+        matriz.append(linhaArray)
+    return matriz
