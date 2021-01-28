8f968e24fa7583a08daa48879f0336b1caf6229d
Diogo de Souza <diogo@sozua.dev>
2021-01-28 13:30:22 -0300

    27 - Strings

--- exercicio.py
+++ exercicio.py
@@ -1,15 +1,12 @@
-def minMax(array):
-    minima = 0
-    maxima = 0
-    for temperatura in array:
-        if temperatura < minima:
-            minima = temperatura
-        if temperatura > maxima:
-            maxima = temperatura
-
-    print("A menor temperatura desse mês foi:", minima)
-    print("A maior temperatura desse mês foi:", maxima)
+def nomeMaisCurto(namesArray):
+    maisCurto = ""
+    for rawName in namesArray:
+        name = rawName.strip().capitalize()
+        if(len(name) < len(maisCurto) or len(maisCurto) == 0):
+            maisCurto = name
+    return maisCurto
 
 
-temperaturas = [12, 32, 23, 43, -1, 2, 4]
-minMax(temperaturas)
+names = ["Juliana",    "Pedro", "   jó    "]
+shortestName = nomeMaisCurto(names)
+print(shortestName)
