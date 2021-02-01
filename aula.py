dff425174c31ec0acb59573b143fc00d547a17d7
Diogo de Souza <diogo@sozua.dev>
2021-02-01 18:48:46 -0300

    36 - Busca Sequencial

--- aula.py
+++ aula.py
@@ -1,35 +1,11 @@
-def criarMatriz(linhas, colunas, valor):
-    matriz = []
-
-    for _ in range(linhas):
-        linha = []
-        for i in range(colunas):
-            if valor:
-                linha.append(valor)
-            else:
-                linha.append(i)
-
-        matriz.append(linha)
-
-    return matriz
+def busca_sequencial(seq, x):
+    for i in range(len(seq)):
+        if seq[i] == x:
+            return True
+    return False
 
 
-def multiplicar_matrizes(a, b):
-    num_linhas_a, num_colunas_a, = len(a), len(a[0])
-    num_linhas_b, num_colunas_b, = len(b), len(b[0])
-    assert num_colunas_a == num_linhas_b
+array = [1, 2, 3, 4, 5, 6]
+encontrou = busca_sequencial(array, 12)
 
-    c = []
-    for linha in range(num_linhas_a):
-        c.append([])
-        for coluna in range(num_colunas_b):
-            c[linha].append(0)
-            for k in range(num_colunas_a):
-                c[linha][coluna] += a[linha][k] * b[k][coluna]
-    return c
-
-
-arrayA = [[1, 2, 3], [4, 5, 6]]
-arrayB = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-arrayC = multiplicar_matrizes(arrayA, arrayB)
-print(arrayC)
+print(encontrou)
