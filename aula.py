c3c9e6ec1a65cbd57db5c2be66239d01e37c8007
Diogo de Souza <diogo@sozua.dev>
2021-02-02 14:38:40 -0300

    41 - Melhoria no algoritmo da Bolha

--- aula.py
+++ aula.py
@@ -1,3 +1,7 @@
+import random
+import time
+
+
 class Ordenador:
     def selecao_direta(self, lista):
         fim = len(lista)
@@ -19,11 +23,51 @@ class Ordenador:
                 if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
 
+    def bolhaOtimizada(self, lista):
+        fim = len(lista)
 
-lista = [10, 3, 9, -2, 200, 17, -8, 32, 12]
+        for i in range(fim - 1, 0, -1):
+            trocou = False
+            for j in range(i):
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+                    trocou = True
+            if not trocou:
+                return
+
+
+def criarLista(n, quaseOrdenado):
+    if quaseOrdenado:
+        lista = [x for x in range(n)]
+        lista[n//10] = -500
+    else:
+        lista = [random.randrange(1000) for x in range(n)]
+    return lista
+
+
+lista1 = criarLista(6000, False)
+lista2 = lista1[:]
+lista3 = lista1[:]
 
 o = Ordenador()
 
-print("Lista desordenada:", lista)
-o.bolha(lista)
-print("Lista ordenada:", lista)
+antesBolha = time.time()
+o.bolha(lista1)
+depoisBolha = time.time()
+
+print("O algoritmo de bolha antigo demorou",
+      depoisBolha - antesBolha, 'segundos')
+
+antesBolhaOtimizada = time.time()
+o.bolha(lista2)
+depoisBolhaOtimizada = time.time()
+
+print("O algoritmo de bolha otimizado demorou",
+      depoisBolhaOtimizada - antesBolhaOtimizada, 'segundos')
+
+antesSelecaoDireta = time.time()
+o.selecao_direta(lista3)
+depoisSelecaoDireta = time.time()
+
+print("O algoritmo de seleção direta demorou",
+      depoisSelecaoDireta - antesSelecaoDireta, 'segundos')
