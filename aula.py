3f1005e40aec1170116e2bcb38b2e5bb6053b784
Diogo de Souza <diogo@sozua.dev>
2021-02-02 13:52:12 -0300

    39 - Algoritmos de Ordenação da Bolha

--- aula.py
+++ aula.py
@@ -11,11 +11,19 @@ class Ordenador:
 
             lista[i], lista[posicao_do_minimo] = lista[posicao_do_minimo], lista[i]
 
+    def bolha(self, lista):
+        fim = len(lista)
+
+        for i in range(fim-1, 0, -1):
+            for j in range(i):
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+
 
 lista = [10, 3, 9, -2, 200, 17, -8, 32, 12]
 
 o = Ordenador()
 
 print("Lista desordenada:", lista)
-o.selecao_direta(lista)
+o.bolha(lista)
 print("Lista ordenada:", lista)
