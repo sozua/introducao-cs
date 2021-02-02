09a706acc34f8699c5dca2bfd86758f37c227e8e
Diogo de Souza <diogo@sozua.dev>
2021-02-02 13:39:06 -0300

    38 - Algoritmos de Ordenação - Seleção Direta

--- aula.py
+++ aula.py
@@ -1,11 +1,21 @@
-def busca_sequencial(seq, x):
-    for i in range(len(seq)):
-        if seq[i] == x:
-            return True
-    return False
+class Ordenador:
+    def selecao_direta(self, lista):
+        fim = len(lista)
+
+        for i in range(fim - 1):
+            posicao_do_minimo = i
+
+            for j in range(i+1, fim):
+                if(lista[j] < lista[posicao_do_minimo]):
+                    posicao_do_minimo = j
+
+            lista[i], lista[posicao_do_minimo] = lista[posicao_do_minimo], lista[i]
 
 
-array = [1, 2, 3, 4, 5, 6]
-encontrou = busca_sequencial(array, 12)
+lista = [10, 3, 9, -2, 200, 17, -8, 32, 12]
 
-print(encontrou)
+o = Ordenador()
+
+print("Lista desordenada:", lista)
+o.selecao_direta(lista)
+print("Lista ordenada:", lista)
